a03756f82eaee8ad4240f370b915307f76210f6d
roano <43611468+roano@users.noreply.github.com>
2019-05-10 15:36:58 +0800

    files

--- gentelella-master/app.js
+++ gentelella-master/app.js
@@ -5,12 +5,28 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const md5 = require('md5');
+var mysql = require('mysql');
 // ---- URL PARSER
 var url = require('url');
 var session = require('express-session');
 // ---- DEFINE SESSION
 server.use(session({secret: 'ssshhhhh'})); 
 // ----
+
+var connection = mysql.createConnection({
+    host: '127.0.0.1:3306',
+    user: 'root',
+    password: '1234',
+    database: 'capstone'
+})
+
+connection.connect(function(error){
+    if(!!error){
+        console.log('Error could not connect');
+    }else{
+        console.log('Connected successfully');
+    }
+})
 
 server.use(express.static( "public" ));
 
