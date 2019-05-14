672b8a4ad9a76b4ef29a90a294fe2a243345d346
roano <rsoriano@shiphealthinc.com>
2019-05-14 11:17:01 +0800

    added comparative analysis page

--- gentelella-master/app.js
+++ gentelella-master/app.js
@@ -48,6 +48,10 @@ server.get('/home', function(req, resp){
     console.log("Testing testing");
 });
 
+server.get('/Comparativeanalysis', function(req, resp){
+   resp.render('./pages/Comparativeanalysis.ejs');
+    console.log("Testing testing");
+});
 
 server.use('/',routes);
 const port = process.env.PORT | 9090;
