4e6ff4b0643ef6384f5e27ea7b6b5cd104bc96d4
AsagiK <42568800+AsagiK@users.noreply.github.com>
2019-06-09 12:07:07 +0800

    Code cleanup

--- gentelella-master/controllers/JSONController.js
+++ gentelella-master/controllers/JSONController.js
@@ -11,22 +11,26 @@ var connection = require('../db');
 var url = require('url');
 var session = require('express-session');
 // ---- DEFINE SESSION
-server.use(session({secret: 'ssshhhhh'})); 
+server.use(session({
+    secret: 'ssshhhhh'
+}));
 // ----
 
 
 module.exports = {
 
-    AssignTaskJSON : function(req, resp){
+    AssignTaskJSON: function (req, resp) {
 
 
-    connection.query("select * from capstone.users", function (err, result, fields){
+        connection.query("select * from capstone.users", function (err, result, fields) {
             if (err) throw err;
-            var preparedresult = JSON.stringify({data: [result]})
+            var preparedresult = JSON.stringify({
+                data: [result]
+            })
             console.log(preparedresult);
             resp.json(preparedresult);
 
         });
     },
-    
+
 }
