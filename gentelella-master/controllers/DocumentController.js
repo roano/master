ba992c2fccd1f0408fe358ea4e820f136d759d90
AsagiK <42568800+AsagiK@users.noreply.github.com>
2019-06-18 19:38:49 +0800

    Doc Controller

---
+++ gentelella-master/controllers/DocumentController.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const server = express();
+const fs = require('fs');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const md5 = require('md5');
+var mysql = require('mysql');
+var connection = require('../db');
+// ---- URL PARSER
+var url = require('url');
+var session = require('express-session');
+// ---- DEFINE SESSION
+server.use(session({
+    secret: 'ssshhhhh'
+}));
+// ----
+
+
+module.exports = {
+    
+     UploadDocument: function (req, resp) {
+        resp.render('./pages/UploadDocument.ejs');
+        console.log("Testing testing");
+    },
+
+    SendDocument: function (req, resp) {
+        var name = req.body.DocName;
+        var filename = req.files.DocFile.name;
+        var path = 'public/uploads/' + req.files.DocFile.name
+        var desc = req.body.DocDesc;
+        console.log(path);
+
+        let uploadedimg = req.files.DocFile;
+        uploadedimg.mv('public/uploads/' + req.files.DocFile.name, function (err) {
+            if (err) return console.log(err);
+            else console.log("File uploaded");
+        })
+
+        var sql = "INSERT INTO `capstone`.`documents` (`Document_Name`, `Document_Route`, `Document_Desc`) VALUES (? , ? , ?);"
+        var values = [name, path, desc];
+
+        connection.query(sql, values, function (err, result) {
+            if (err) throw err;
+            console.log("Record Inserted");
+
+        });
+
+
+        resp.redirect('/UploadDocument');
+
+    },
+
+}
--- gentelella-master/routes.js
+++ gentelella-master/routes.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var planCtrl = require('./controllers/planController');
 var JSONCtrl = require('./controllers/JSONController');
+var DocCtrl = require('./controllers/DocumentController');
 var router = express.Router();
 
 router.route('/Viewusers').get(planCtrl.Viewusers);
@@ -17,10 +18,11 @@ router.route('/add-group').post(planCtrl.addgroup);
 router.route('/adduser').post(planCtrl.adduser);
 router.route('/edituser').get(planCtrl.edituser);
 router.route('/alteruser').post(planCtrl.alteruser);
-router.route('/UploadDocument').get(planCtrl.UploadDocument);
-router.route('/SendDocument').post(planCtrl.SendDocument);
 router.route('/Recommendations').get(planCtrl.Recommendations);
 
 router.route('/AssignTaskJSON').post(JSONCtrl.AssignTaskJSON);
+
+router.route('/UploadDocument').get(DocCtrl.UploadDocument);
+router.route('/SendDocument').post(DocCtrl.SendDocument);
 
 module.exports = router;
