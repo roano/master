e436547dc88070dc531c26e091e4028659e20a5d
roano <43611468+roano@users.noreply.github.com>
2019-06-29 21:34:45 +0800

    Planning Page can select groups in the database

--- gentelella-master/controllers/planController.js
+++ gentelella-master/controllers/planController.js
@@ -260,9 +260,14 @@ module.exports = {
     },
 
     Planning: function (req, resp) {
-        resp.render('./pages/PlanPage.ejs');
-        console.log("PlanPage");
-
+        connection.query("Select * FROM capstone.plans; Select group.Group_ID, group.Group_Name, area.Area_Name FROM capstone.group join capstone.area on group.Area_ID = area.Area_ID;", function (err, results, fields) {
+            if(err) throw err;    
+            resp.render('./pages/PlanPage.ejs', {
+                data: results[0],
+                dataB: results[1]
+            });
+            console.log(results);
+        });
     },
 
     RecommendationNonAjax: function (req, resp) {
