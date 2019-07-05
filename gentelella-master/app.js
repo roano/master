b74f8cd7d448cdcec6155aebe705bc67a6f309e3
Lucinia <41266206+Lucinia@users.noreply.github.com>
2019-07-05 10:09:39 +0800

    all things done except ajax
    Ajax not done yet fuckin default image took most of my time. the background image only works if I set the default page as html

--- gentelella-master/app.js
+++ gentelella-master/app.js
@@ -35,7 +35,7 @@ server.set('view engine', 'ejs');
 
 server.get('/', function (req, resp) {
 
-    resp.render('./pages/ViewTaskDetails.ejs');
+    resp.render('./pages/defaultpage.html');
 
     console.log("Testing testing");
 });
