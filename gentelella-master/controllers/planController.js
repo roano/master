d3db5924e6b931f7fe338ad9aeffff4c6bc2f8df
AsagiK <42568800+AsagiK@users.noreply.github.com>
2019-07-06 02:24:40 +0800

    Users Improvements

--- gentelella-master/controllers/planController.js
+++ gentelella-master/controllers/planController.js
@@ -22,7 +22,7 @@ module.exports = {
 
     Viewusers: function (req, resp) {
 
-        connection.query("SELECT users.User_ID, users.User_First, users.User_Last, users.email_address, group.Group_Name, roles.Role_Name, users.ContactNo FROM capstone.users join capstone.group on users.Group=group.Group_ID join capstone.roles on users.Role = roles.Role_ID; SELECT users.Role, users.User_ID, users.User_First, users.User_Last, users.email_address, users.ContactNo FROM capstone.users where users.Group IS NULL; ", function (err, results, fields) {
+        connection.query("SELECT users.User_ID, users.User_First, users.Role, users.User_Last, users.email_address, group.Group_Name, area.Area_Name, roles.Role_Name, users.ContactNo FROM capstone.users join capstone.group on users.Group=group.Group_ID join capstone.roles on users.Role = roles.Role_ID join capstone.area on group.Area_ID = area.Area_ID; SELECT users.Role, users.User_ID, users.User_First, users.User_Last, users.email_address, users.ContactNo FROM capstone.users where users.Group IS NULL; ", function (err, results, fields) {
             if (err) throw err;
             resp.render('./pages/Viewusers.ejs', {
                 data: results[0],
